4429ee86538d3f2e535013261235b4cca5af92c0
Aseelsamer <assoul_08@hotmail.com>
2020-11-20 21:15:14 +0200

    bla

--- js/app.js
+++ js/app.js
@@ -1,106 +1,135 @@
-// 'use strict;'
+'use strict';
+//Array of Photo objects
+let allPhotos = [];
 
-// // var cat={
-// //     name:'sherry',
-// //     age:0,
-// //     likes:'string and napping',
-// //     goodWithCats:'yes',
-// //     goodWithKids:'no',
-// //     goodWithotherCats:'yes',
-// //     breed:'egyption',
-// //     imagePath:
-// //     // getAge:function(min,max){
-// //     //     this.age=getRandomNumber(min,max);
-// //     // }
-// // }
+//Constructor
+function Photo(photoObj) {
+    this.image_url = photoObj.image_url;
+    this.title = photoObj.title;
+    this.description = photoObj.description;
+    this.keyword = photoObj.keyword;
+    this.horns = photoObj.horns;
+    allPhotos.push(this);
+}
+//Prototype to render the images and their title and description
+Photo.prototype.render = function () {
+    // let templete = $('#photo-template').clone();
+    // templete.find('h2').text(this.title);
+    // templete.find('img').attr('src', this.image_url);
+    // templete.find('p').text(this.description);
+    // templete.remove('photo-template');
 
-// var cat1={
-//     name:'lulu',
-//     age:0,
-//     likes:'cuddling and napping',
-//     goodWithCats:false,
-//     goodWithKids:true,
-//     imagePath:`img/cat1.png`,
-//     goodWithotherCats:true,
-//     breed:'persian',
-//     getAge:function(min,max){
-//         this.age=getRandomNumber(min,max);
-//     },
-//     render:  function(){
-//         var container=document.getElementById('kitten');
-//         var article=document.createElement('article');
-//     container.appendChild(article);
+    let template=$("#templateMain").html();
+    let sectionMain=Mustache.render(template,this);
+    $('main').append(sectionMain);
+}
+//Array to store the selected photos from select
+var selectedPhotosArr = [];
+$('#page1').click(()=>{
+    $('select').html('');
+    allPhotos=[];
+    selectedPhotosArr=[];
+    $('select').append('<option value="default">Filter By Keyword</option>');
+    $("section").remove();
+});
+//To get the data from the JSON file
+$.ajax('./data/page-1.json')
+    .then(data => {
+        console.log(data); // array of objects
+        data.forEach((value) => {
+            //Create a new object of type Photo
+            let newPhoto = new Photo(value);
+            newPhoto.render();
+            //Check if the selected photo is in the array or not
+            if (!selectedPhotosArr.includes(value.keyword)) {
+                selectedPhotosArr.push(value.keyword);
+            }
+        });
+        $('#photo-template').first().remove();
+        selectPhoto();
+    });
+
+    $('#page2').click(()=>{
+        $('select').html('');
+        allPhotos=[];
+        selectedPhotosArr=[];
+        $('select').append('<option value="default">Filter By Keyword</option>');
+        $("section").remove();
+    });
+
     
-//     var header= document.createElement('h2');
-//     article.appendChild(header);
-//     header.textContent = this.name;
-    
-//     var para=document.createElement('p');
-//     article.appendChild(para);
-//     para.textContent=`${name} is adorable and is ${this.age} months old.`
-    
-//     var ulE1=document.createElement('ul');
-//     article.appendChild(ulE1);
-    
-    
-//     var list=document.createElement('li');
-//     ulE1.appendChild(list);
-//     list.textContent=this.likes;
-
-//     var img=document.createElement('img');
-//     article.appendChild(img);
-//     img.setAttribute('src',this.imagePath);
-    
-//     }
-//     }
+    $.ajax('./data/page-2.json')
+    .then(data => {
+        console.log(data); // array of objects
+        data.forEach((value) => {
+            //Create a new object of type Photo
+            let newPhoto = new Photo(value);
+            newPhoto.render();
+            //Check if the selected photo is in the array or not
+            if (!selectedPhotosArr.includes(value.keyword)) {
+                selectedPhotosArr.push(value.keyword);
+            }
+        });
+        $('#photo-template').first().remove();
+        selectPhoto();
+    });
 
 
-// var cat2={
-//     name:'tofi',
-//     age:0,
-//     likes:'chasing string ',
-//     goodWithCats:true,
-//     goodWithKids:false,
-//     goodWithotherCats:true,
-//     imagePath:`img/cat2.jpg`,
-//     breed:'british',
-//     getAge:function(min,max){
-//         this.age=getRandomNumber(min,max);
-//     },
-    
-    
-// render:  function(){
-//     var container=document.getElementById('kitten');
-//     var article=document.createElement('article');
-// container.appendChild(article);
+    $('#sortHorns').click(()=>{
+        $('main').html('');
+        sotrHorns();
+        allPhotos.forEach(val=>{
+            val.render();
+        })
 
-// var header= document.createElement('h2');
-// article.appendChild(header);
-// header.textContent = this.name;
+    });
 
-// var para=document.createElement('p');
-// article.appendChild(para);
-// para.textContent=`${name} is adorable and is ${this.age} months old.`
+    function sotrHorns(){
+        allPhotos.sort((a,b)=>{
+          if(a.horns > b.horns){
+            return 1;
+          }else if (a.horns<b.horns){
+            return -1;
+          }else {return 0;}
+        });
+      }
 
-// var ulE1=document.createElement('ul');
-// article.appendChild(ulE1);
+    $('#sortTitle').click(()=>{
+        $('main').html('');
+        sortTitle();
+        allPhotos.forEach(val=>{
+            val.render();
+        })
 
-
-// var list=document.createElement('li');
-// ulE1.appendChild(list);
-// list.textContent=this.likes;
-
-
-// var img=document.createElement('img');
-//     article.appendChild(img);
-//     img.setAttribute('src',this.imagePath);
-// }
-// }
-
-// cat2.getAge(1,9);
-// cat2.render();
-// cat1.render();
-// cat1.getAge(2,8);
-// function getRandomNumber(min, max) {
-//  return Math.floor(Math.random() * (max - min + 1)) + min;
-//  }
+    });
+    function sortTitle(){
+        allPhotos.sort((a,b)=>{
+          if(a.title.toUpperCase() > b.title.toUpperCase()){
+            return 1;
+          }else if (a.title.toLowerCase() < b.title.toLowerCase()){
+            return -1;
+          }else {return 0;}
+        });
+      }
+    //Function to add options to the 'select' element from the selectedPhotosArr array
+    function selectPhoto() {
+    selectedPhotosArr.forEach(element => {
+        let option = $(`<option value="${element}"> ${element}</option>`);
+        console.log(option);
+        $('select').append(option);
+    });
+}
+$('select').click(function () {
+    let selected = $(this).val();
+    //As long as the value of the option selected is not default
+    if (selected !== 'default') {
+        let container = $('#photo-template');
+        $('main').empty();
+        $('main').append(container);
+        allPhotos.forEach(function (value) {
+            if (value.keyword === selected) {
+                value.render();
+            }
+        });
+    }
+});
